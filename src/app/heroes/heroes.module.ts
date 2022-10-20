ae2612eded78ef20a583892ccb95d87c8a4e9407
MECVxD <mauricioecovarrubias@hotmail.com>
2022-10-19 22:35:35 -0500

    Saved changes class 198

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
@@ -26,6 +27,7 @@ import { ImagenPipe } from './pipes/imagen.pipe';
   imports: [
     CommonModule,
     FlexLayoutModule,
+    FormsModule,
     HeroesRoutingModule,
     MaterialModule,
   ],
--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-buscar',
   templateUrl: './buscar.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class BuscarComponent implements OnInit {
+  public termino: string = '';
+  public heroes: Heroe[] = [];
 
-  constructor() { }
+  constructor(private heroesService: HeroesService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public buscando() {
+    this.heroesService
+      .getHeroes()
+      .subscribe((heroes: Heroe[]) => (this.heroes = heroes));
   }
-
 }
