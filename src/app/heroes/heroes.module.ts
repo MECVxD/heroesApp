4a3762e1fd4b0a806b10d1a4647c9d3c018a0d9b
Mauricio Covarrubias <mauricioecovarrubias@hotmail.com>
2024-09-03 13:33:06 -0600

    Added last changes for section 14

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -6,19 +6,22 @@ import { HeroesRoutingModule } from './heroes-routing.module';
 import { MaterialModule } from '../material/material.module';
 
 import { HeroImagePipe } from './pipes/hero-image.pipe';
+
+import { HeroCardComponent } from './components/hero-card/hero-card.component';
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
-import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
-import { HeroCardComponent } from './components/hero-card/hero-card.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
 
 @NgModule({
   declarations: [
+    HeroCardComponent,
     HeroPageComponent,
-    SearchPageComponent,
     LayoutPageComponent,
     ListPageComponent,
-    HeroCardComponent,
+    NewPageComponent,
+    SearchPageComponent,
 
     HeroImagePipe
   ],
